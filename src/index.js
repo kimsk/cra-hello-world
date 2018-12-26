86cbe8dc6adafc9ff0c78f83f3ca4a59d7564c41
Karlkim Suwamongkol <contact@karlk.im>
2018-12-26 13:19:56 -0600

    blockstack with create-react-app

--- src/index.js
+++ src/index.js
@@ -4,8 +4,20 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { isUserSignedIn, loadUserData, isSignInPending, handlePendingSignIn, redirectToSignIn } from 'blockstack'
 
+if (isUserSignedIn()) {
+  console.log("signed in")
+  var profile = loadUserData().profile
+  console.log(profile)
+  ReactDOM.render(<App />, document.getElementById('root'))
+} else if (isSignInPending()) {
+  handlePendingSignIn().then(function(userData) {
+    window.location = window.location.origin
+  })
+} else {
+  redirectToSignIn()
+}
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
