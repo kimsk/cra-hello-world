d5e15eca7ec97a7239c0b129409df5bb150de85d
Karlkim Suwamongkol <contact@karlk.im>
2018-12-26 22:00:21 -0600

    login & logout buttons

---
+++ src/components/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import Profile from './Profile'
+import Signin from './Signin'
+import {
+  isSignInPending,
+  isUserSignedIn,
+  redirectToSignIn,
+  handlePendingSignIn,
+  signUserOut,
+} from 'blockstack'
+
+export default class App extends Component {
+  handleSignIn(e) {
+    e.preventDefault()
+    redirectToSignIn()
+  }
+
+  handleSignOut(e) {
+    e.preventDefault()
+    signUserOut(window.location.origin)
+  }
+
+  render() {
+    return (
+      <div className="site-wrapper">
+        <div className="site-wrapper-inner">
+          { !isUserSignedIn() ?
+            <Signin handleSignIn={ this.handleSignIn } />
+            : <Profile handleSignOut={ this.handleSignOut } />
+          }
+        </div>
+      </div>
+    )
+  }
+
+  componentWillMount() {
+    if (isSignInPending()) {
+      handlePendingSignIn().then((userData) => {
+        window.location = window.location.origin
+      })
+    }
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,24 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react' 
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
 
-import { isUserSignedIn, loadUserData, isSignInPending, handlePendingSignIn, redirectToSignIn } from 'blockstack'
 
-if (isUserSignedIn()) {
-  console.log("signed in")
-  var profile = loadUserData().profile
-  console.log(profile)
-  ReactDOM.render(<App />, document.getElementById('root'))
-} else if (isSignInPending()) {
-  handlePendingSignIn().then(function(userData) {
-    window.location = window.location.origin
-  })
-} else {
-  redirectToSignIn()
-}
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(<App />, document.getElementById('root'))
